ab91078ca7e18227e68219211ae85bc48f4d4775
unknown <edward4992@gmail.com>
2024-07-02 17:41:31 +0900

    실습

---
+++ src/zoo/FourFiveJung.java
@@ -0,0 +1,8 @@
+package zoo;
+
+public class FourFiveJung extends Animal {
+	@Override
+	void makeSomeNoise() {
+		System.out.println("나바아아아아앙~~~");
+	}
+}
---
+++ src/zoo/Lizard.java
@@ -0,0 +1,8 @@
+package zoo;
+
+public class Lizard extends Animal {
+	@Override
+	void makeSomeNoise() {
+		System.out.println("니코 아니지롱~~");
+	}
+}
